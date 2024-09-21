6f66cd695a08205a49dce8d44171e6a6e244c0ea
ddubbu-dev <dev.ddubbu@gmail.com>
2024-09-21 13:33:17 +0000

    feat(seq-proxy): client < proxy < tiny

--- proxy.c
+++ proxy.c
@@ -42,26 +42,40 @@ void action(int client_proxy_fd) {
     sscanf(client_request, "%s %s %s", method, uri, version);
 
     if (strcmp(method, "GET") != 0) { //  (simple) http request 유효성 체크
-        char err_msg[MAXLINE] = "NOT VALID request: Allow only [GET] method";
-        Rio_writen(client_proxy_fd, err_msg, strlen(err_msg));
+        char err_msg[MAXLINE] = "NOT VALID request: Allow only [GET] method\r\n\r\n";
+        Rio_writen(client_proxy_fd, err_msg, strlen(err_msg)); // TODO: HTTP err ststus
         return;
     }
 
     char path[MAXBUF], hostname[MAXBUF], port[MAXBUF];
     update_tiny_info(uri, path, hostname, port); // uri parse를 통한 정보 업데이트
 
-    printf("===========[Proxy --(Request)--> Tiny]===========\n");
+    printf("===========[Proxy  --(Request)-->  Tiny]===========\n");
     int proxy_tiny_fd = Open_clientfd(hostname, port);
     char http_first_line[MAXBUF];
     sprintf(http_first_line, "%s %s %s\r\n", method, path, version);
     Rio_writen(proxy_tiny_fd, http_first_line, strlen(http_first_line)); // client > proxy --(req)-> tiny
     printf("[req] %s", http_first_line);
 
-    printf("===========[Proxy <--(Response)-- Tiny]===========\n");
-    while (Rio_readlineb(&client_rio, client_request, MAXBUF) > 0) {       // client --(req)-> proxy > tiny
-        Rio_writen(proxy_tiny_fd, client_request, strlen(client_request)); // client > proxy --(req)-> tiny
+    while ((readn = Rio_readlineb(&client_rio, client_request, MAXBUF)) > 0) { // client --(req)-> proxy > tiny
+        Rio_writen(proxy_tiny_fd, client_request, readn);                      // client > proxy --(req)-> tiny
+
+        if (strcmp(client_request, "\r\n") == 0) {
+            break;
+        }
         printf("[req] %s", client_request);
     }
+    printf("===========[Proxy  <--(Response)--  Tiny]===========\n");
+    printf("===========[Client <--(Response)-- Proxy]===========\n");
+    char server_response[MAXBUF];
+    rio_t proxy_rio;
+    Rio_readinitb(&proxy_rio, proxy_tiny_fd);
+    while ((readn = Rio_readlineb(&proxy_rio, server_response, MAXBUF)) > 0) { // client > proxy <-(res)-- tiny
+        Rio_writen(client_proxy_fd, server_response, readn);                   // client <-(res)-- proxy < tiny
+        printf("[res] %s", server_response);
+    }
+
+    printf("===========[END]===========\n");
 }
 
 void update_tiny_info(char *uri, char *path, char *hostname, char *port) {
