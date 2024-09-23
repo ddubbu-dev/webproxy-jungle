bfa982469c345996846df83011377607b174eae7
ddubbu-dev <dev.ddubbu@gmail.com>
2024-09-23 01:57:23 +0000

    chore: save

--- proxy.c
+++ proxy.c
@@ -45,6 +45,7 @@ void *thread(void *vargp) {
 RequestInfo *req_p;
 void action(int client_proxy_fd) {
     char client_request[MAXBUF], method[MAXBUF], uri[MAXBUF], version[MAXBUF];
+    char path[MAXBUF], hostname[MAXBUF], port[MAXBUF];
 
     printf("===========[Client --(Request)--> Proxy]===========\n");
     rio_t client_rio;
@@ -53,37 +54,46 @@ void action(int client_proxy_fd) {
     int readn = Rio_readlineb(&client_rio, client_request, MAXBUF); // client --(req)-> proxy > tiny
     sscanf(client_request, "%s %s %s", method, uri, version);
 
+    // 1. cache 존재 여부 체크
+    // Yes: 캐시 사용
+    RequestInfo *req_p = (RequestInfo *)malloc(sizeof(RequestInfo));
+
+    update_tiny_info(uri, path, hostname, port); // uri parse를 통한 정보 업데이트
+    strcpy(req_p->method, method);
+    req_p->path = path;
+
+    printf("method=%s, path=%s\n", method, path);
+
+    CacheNode *cache_node = search(dll, req_p);
+
+    printf("cache_node=%p\n", cache_node);
+
+    if (cache_node != NULL) {
+        // TODO: 어쩔 수 없이
+        Rio_writen(client_proxy_fd, "Server: Hello\r\n", strlen("Server: Hello\r\n"));
+        Rio_writen(client_proxy_fd, "\r\n", strlen("\r\n"));
+        Rio_writen(client_proxy_fd, cache_node->res_p, strlen(cache_node->res_p)); // body only
+
+        printf(cache_node->res_p);
+
+        moveFront(dll, cache_node);
+        return;
+    }
+
+    // 2. No: 요청 새로 보내기 > 캐시 저장
+    printf("===========[Proxy  --(Request)-->  Tiny]===========\n");
     if (strcmp(method, "GET") != 0) { //  (simple) http request 유효성 체크
         char err_msg[MAXLINE] = "NOT VALID request: Allow only [GET] method\r\n\r\n";
         Rio_writen(client_proxy_fd, err_msg, strlen(err_msg)); // TODO: HTTP err ststus
         return;
     }
 
-    char path[MAXBUF], hostname[MAXBUF], port[MAXBUF];
-    update_tiny_info(uri, path, hostname, port); // uri parse를 통한 정보 업데이트
-
-    printf("===========[Proxy  --(Request)-->  Tiny]===========\n");
     int proxy_tiny_fd = Open_clientfd(hostname, port);
+
     char http_first_line[MAXBUF];
     sprintf(http_first_line, "%s %s %s\r\n", method, path, version);
-
-    // 1. cache 존재 여부 체크
-    // Yes: 캐시 사용
-    RequestInfo *req_p = (RequestInfo *)malloc(sizeof(RequestInfo));
-    strcmp(req_p->method, method);
-    req_p->path = path;
-
-    CacheNode *cache_node = search(dll, req_p);
-
-    if (cache_node != NULL) {
-        Rio_writen(proxy_tiny_fd, cache_node->res_p, strlen(cache_node->res_p));
-        moveFront(dll, cache_node);
-        return;
-    }
-
-    // No: 요청 새로 보내기 > 캐시 저장
+    printf("[req]http_first_line %s", http_first_line);
     Rio_writen(proxy_tiny_fd, http_first_line, strlen(http_first_line)); // client > proxy --(req)-> tiny
-    printf("[req] %s", http_first_line);
 
     while ((readn = Rio_readlineb(&client_rio, client_request, MAXBUF)) > 0) { // client --(req)-> proxy > tiny
         Rio_writen(proxy_tiny_fd, client_request, readn);                      // client > proxy --(req)-> tiny
@@ -109,7 +119,7 @@ void action(int client_proxy_fd) {
         Rio_writen(client_proxy_fd, server_response_header, readn);                   // client <-(res)-- proxy < tiny
         printf("[res] %s", server_response_header);
 
-        if (server_response_header == "\r\n") {
+        if (strcmp(server_response_header, "\r\n") == 0) {
             break; // header 읽기 끝!
         }
 
@@ -118,14 +128,16 @@ void action(int client_proxy_fd) {
         }
     }
 
+    printf("\n");
+
     // body 읽기
     char server_response_body[content_length];
-
     Rio_readnb(&proxy_rio, server_response_body, content_length);
     Rio_writen(client_proxy_fd, server_response_body, content_length); // client <-(res)-- proxy < tiny
 
     req_p = (RequestInfo *)malloc(sizeof(RequestInfo));
     strcpy(req_p->method, method);
+    req_p->path = path;
     pushFront(dll, req_p, server_response_body);
 }
 
